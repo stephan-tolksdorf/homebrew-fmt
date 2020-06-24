371d55c7a918b2d6e9cec1a28244a7b5a164bfc2
Stephan Tolksdorf <st@quanttec.com>
2020-06-24 15:03:59 +0200

    Add stable url

--- HomebrewFormula/fmt.rb
+++ HomebrewFormula/fmt.rb
@@ -3,6 +3,11 @@ class Fmt < Formula
   homepage "https://github.com/fmtlib/fmt"
   head "git://github.com/fmtlib/fmt.git", :using => :git
 
+  stable do
+    url "https://github.com/fmtlib/fmt/archive/31c3a24.tar.gz" # https://github.com/fmtlib/fmt/commit/31c3a24266ed517cb0ddac639ceab777d14d104a
+    sha256 "be7ab0d9f60bb42d16728f356a68bc4eea8937442e6f6aebf6639f26bc810168"
+  end
+
   depends_on "cmake" => :build
 
   def install
